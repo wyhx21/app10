2c0ec42abb2280e69acdecae0f0217949a4c8e03
wyhx21 <2247819253@qq.com>
2021-01-06 02:23:19 +0800

    添加标题

--- src/router/package/dict.js
+++ src/router/package/dict.js
@@ -2,21 +2,25 @@ const prodType = [
   {
     path: "/dict/prodType",
     name: "dict_prodType",
+    meta: { pageName: "商品类型" },
     component: () => import("@views/dict/prodType/Index.vue")
   },
   {
     path: "/dict/prodTypeDetail",
     name: "dict_prodType_detail",
+    meta: { pageName: "商品类型-详情" },
     component: () => import("@views/dict/prodType/Detail.vue")
   },
   {
     path: "/dict/prodTypeEditor",
     name: "dict_prodType_editor",
+    meta: { pageName: "商品类型-编辑" },
     component: () => import("@views/dict/prodType/Editor.vue")
   },
   {
     path: "/dict/prodTypePersist",
     name: "dict_prodType_persist",
+    meta: { pageName: "商品类型-新增" },
     component: () => import("@views/dict/prodType/Persist.vue")
   }
 ];
@@ -25,21 +29,25 @@ const verifyType = [
   {
     path: "/dict/verifyType",
     name: "dict_verifyType",
+    meta: { pageName: "核销类型" },
     component: () => import("@views/dict/verifyType/Index.vue")
   },
   {
     path: "/dict/verifyTypeDetail",
     name: "dict_verifyType_detail",
+    meta: { pageName: "核销类型-详情" },
     component: () => import("@views/dict/verifyType/Detail.vue")
   },
   {
     path: "/dict/verifyTypeEditor",
     name: "dict_verifyType_editor",
+    meta: { pageName: "核销类型-编辑" },
     component: () => import("@views/dict/verifyType/Editor.vue")
   },
   {
     path: "/dict/verifyTypePersist",
     name: "dict_verifyType_persist",
+    meta: { pageName: "核销类型-新增" },
     component: () => import("@views/dict/verifyType/Persist.vue")
   }
 ];
--- src/router/package/order.js
+++ src/router/package/order.js
@@ -2,36 +2,43 @@ const purchaseOrder = [
   {
     path: "/order/purchase",
     name: "order_purchase",
+    meta: { pageName: "采购订单" },
     component: () => import("@views/order/purchaseOrder/Index.vue")
   },
   {
     path: "/order/purchaseParam",
     name: "order_purchase_param",
+    meta: { pageName: "采购订单-筛选" },
     component: () => import("@views/order/purchaseOrder/QueryParam.vue")
   },
   {
     path: "/order/purchasePersist",
     name: "order_purchase_persist",
+    meta: { pageName: "采购订单-新增" },
     component: () => import("@views/order/purchaseOrder/Persist.vue")
   },
   {
     path: "/order/purchaseDetail",
     name: "order_purchase_detail",
+    meta: { pageName: "采购订单-详情" },
     component: () => import("@views/order/purchaseOrder/Detail.vue")
   },
   {
     path: "/order/purchaseSupplier",
     name: "order_purchase_supplier",
+    meta: { pageName: "采购订单-供应商" },
     component: () => import("@views/order/purchaseOrder/Supplier.vue")
   },
   {
     path: "/order/purchaseProduct",
     name: "order_purchase_product",
+    meta: { pageName: "采购订单-商品" },
     component: () => import("@views/order/purchaseOrder/Product.vue")
   },
   {
     path: "/order/purchaseStore",
     name: "order_purchase_store",
+    meta: { pageName: "采购订单-入库详情" },
     component: () => import("@views/order/purchaseOrder/DetailStore.vue")
   }
 ];
@@ -40,36 +47,43 @@ const saleOrder = [
   {
     path: "/order/sale",
     name: "order_sale",
+    meta: { pageName: "销售订单" },
     component: () => import("@views/order/saleOrder/Index.vue")
   },
   {
     path: "/order/saleParam",
     name: "order_sale_param",
+    meta: { pageName: "销售订单-筛选" },
     component: () => import("@views/order/saleOrder/QueryParam.vue")
   },
   {
     path: "/order/saleDetail",
     name: "order_sale_detail",
+    meta: { pageName: "销售订单-详情" },
     component: () => import("@views/order/saleOrder/Detail.vue")
   },
   {
     path: "/order/salePersist",
     name: "order_sale_persist",
+    meta: { pageName: "销售订单-新增" },
     component: () => import("@views/order/saleOrder/Persist.vue")
   },
   {
     path: "/order/saleCustomer",
     name: "order_sale_customer",
+    meta: { pageName: "销售订单-客户" },
     component: () => import("@views/order/saleOrder/Customer.vue")
   },
   {
     path: "/order/saleProduct",
     name: "order_sale_product",
+    meta: { pageName: "销售订单-商品" },
     component: () => import("@views/order/saleOrder/Product.vue")
   },
   {
     path: "/order/saleStore",
     name: "order_sale_store",
+    meta: { pageName: "销售订单-出库详情" },
     component: () => import("@views/order/saleOrder/DetailStore.vue")
   }
 ];
