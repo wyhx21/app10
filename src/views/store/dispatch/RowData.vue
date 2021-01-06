01492092f78bf093b306d6080d4327f306268a81
wyhx21 <2247819253@qq.com>
2021-01-06 11:06:31 +0800

    update

--- src/views/store/dispatch/RowData.vue
+++ src/views/store/dispatch/RowData.vue
@@ -1,7 +1,10 @@
 <template>
-  <table width="100%">
+  <table class="app-data-item-table">
     <tr>
-      <td colspan="2">{{ data["orderNo"] }}</td>
+      <td colspan="2">
+        <em>订单号</em>
+        <span>{{ data["orderNo"] }}</span>
+      </td>
       <td>
         <span class="app-link" @click="gotoDetail()" v-if="showDetail"
           >详情</span
@@ -11,31 +14,38 @@
     <tr>
       <td width="40%"><span>调出仓库</span></td>
       <td width="30%">
+        <em>调出仓库编码</em>
         <span>{{ data["storeCode"] }}</span>
       </td>
-      <td>
+      <td colspan="2">
+        <em>调出仓库名称</em>
         <span>{{ data["storeName"] }}</span>
       </td>
     </tr>
     <tr>
       <td><span>调入仓库</span></td>
       <td>
+        <em>调入仓库编码</em>
         <span>{{ data["toStoreCode"] }}</span>
       </td>
       <td>
+        <em>调入仓库名称</em>
         <span>{{ data["toStoreName"] }}</span>
       </td>
     </tr>
     <tr>
       <td>
+        <em>更新人</em>
         <span>{{ data["lastModifiedBy"] }}</span>
       </td>
       <td colspan="2">
+        <em>更新时间</em>
         <span>{{ data["lastModifiedDate"] }}</span>
       </td>
     </tr>
     <tr>
       <td>
+        <em>备注</em>
         <p class="app-data-item_remark">{{ data["toStoreName"] }}</p>
       </td>
     </tr>
--- src/views/store/verify/DetailItem.vue
+++ src/views/store/verify/DetailItem.vue
@@ -1,18 +1,22 @@
 <template>
-  <table width="100%">
+  <table class="app-data-item-table">
     <tr>
       <td width="50%">
+        <em>区域编码</em>
         <span>{{ data["areaCode"] }}</span>
       </td>
       <td>
+        <em>区域名称</em>
         <span>{{ data["areaName"] }}</span>
       </td>
     </tr>
     <tr>
       <td>
+        <em>商品编码</em>
         <span>{{ data["prodCode"] }}</span>
       </td>
       <td>
+        <em>商品名称</em>
         <span>{{ data["prodName"] }}</span>
       </td>
     </tr>
@@ -21,6 +25,7 @@
         <span>核销数量</span>
       </td>
       <td>
+        <em>核销数量</em>
         <span>{{ data["prodNum"] }}({{ data["prodUnit"] }})</span>
       </td>
     </tr>
@@ -30,11 +35,13 @@
         <span>改变数量</span>
       </td>
       <td>
+        <em>改变数量</em>
         <span>{{ data["changeNum"] }}({{ data["prodUnit"] }})</span>
       </td>
     </tr>
     <tr>
       <td colspan="2">
+        <em>备注信息</em>
         <p class="app-data-item_remark">{{ data["remark"] }}</p>
       </td>
     </tr>
